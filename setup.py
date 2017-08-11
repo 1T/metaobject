7c0ddcf03bfc5c48ba56bcbabd9f02437185eebb
Andrew Robbins <and_j_rob@yahoo.com>
2017-08-11 11:05:39 -0400

    Added six as a requirement

--- setup.py
+++ setup.py
@@ -1,25 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# metaobject
-# Copyright (c) 2014, Andrew Robbins, All rights reserved.
-# 
-# This library ("it") is free software; it is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; you can redistribute it and/or modify it under the terms of the
-# GNU Lesser General Public License ("LGPLv3") <https://www.gnu.org/licenses/lgpl.html>.
+# Copyright © 2012-2016 1Ticket.com
 '''
-metaobject - A Meta-Object protocol library
+Standard setup script
 '''
 from __future__ import absolute_import
-from setuptools import setup, find_packages
-import json
-import metaobject
+from setuptools import setup
 
-if __name__ == '__main__':
-    setup_config = json.load(open("package.json"))
-    setup(
-        long_description = open('README.md').read(),
-        packages = find_packages(),
-        version = metaobject.__version__,
-        **setup_config
-    )
+setup(
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=True)
